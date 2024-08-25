d36742b1e9f3132cf8586f0d2092baed16ae937b
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-08-25 18:05:07 -0500

    Refactor project storage to use Map instead of List
    Switching from List to Map in InMemoryProjectRepository improves lookup efficiency by using project names as keys. This change includes modifying methods to accommodate the Map data structure and adding a new findByName method for retrieving projects by name.

--- src/main/java/dev/nathanlively/application/InMemoryProjectRepository.java
+++ src/main/java/dev/nathanlively/application/InMemoryProjectRepository.java
@@ -4,30 +4,37 @@ import dev.nathanlively.application.port.ProjectRepository;
 import dev.nathanlively.domain.Project;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryProjectRepository implements ProjectRepository {
-    private final List<Project> projects;
 
-    public InMemoryProjectRepository(List<Project> projects) {
+    private final Map<String, Project> projects;
+
+    public InMemoryProjectRepository(Map<String, Project> projects) {
         this.projects = projects;
     }
 
-    public static InMemoryProjectRepository create(List<Project> projects) {
+    public static InMemoryProjectRepository create(Map<String, Project> projects) {
         return new InMemoryProjectRepository(projects);
     }
 
     public static ProjectRepository createEmpty() {
-        return create(new ArrayList<>());
+        return create(new HashMap<>());
     }
 
     @Override
     public void save(Project project) {
-        projects.add(project);
+        projects.put(project.name(), project);
     }
 
     @Override
     public List<Project> findAll() {
-        return projects;
+        return new ArrayList<>(projects.values());
+    }
+
+    public Project findByName(String name) {
+        return projects.get(name);
     }
 }
