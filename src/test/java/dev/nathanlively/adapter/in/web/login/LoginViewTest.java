88f6bb231c961ae7c6aecb482966edee3519f1bc
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-09-06 11:20:43 -0500

    Disable Eclipse store debug logging
    Commented out the debug logging configuration for the Eclipse store in the application properties file. Updated the LoginViewTest to include checking the page title after a successful login.

--- src/test/java/dev/nathanlively/adapter/in/web/login/LoginViewTest.java
+++ src/test/java/dev/nathanlively/adapter/in/web/login/LoginViewTest.java
@@ -1,27 +1,24 @@
 package dev.nathanlively.adapter.in.web.login;
 
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.login.LoginOverlay;
 import dev.nathanlively.adapter.in.web.droidcomm.KaribuTest;
+import dev.nathanlively.security.Role;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.github.mvysny.kaributesting.v10.LocatorJ._get;
-import static com.github.mvysny.kaributesting.v10.LoginFormKt._login;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Tag("ui")
 class LoginViewTest extends KaribuTest {
     @Test
     void nathanCanLogin() throws Exception {
+        login("nathanlively@gmail.com", "", List.of(String.valueOf(Role.ADMIN)));
+
         UI.getCurrent().navigate(LoginView.class);
 
-        try {
-            _login(_get(LoginOverlay.class), "not.existing@user.com", "pass");
-        } catch (IllegalStateException e) {
-            // From GoogleAnalyticsTracker. Ignore
-        }
-
-        assertThat(_get(LoginOverlay.class).getElement().getOuterHTML()).isEqualTo("<vaadin-login-overlay></vaadin-login-overlay>");
+        String pageTitle = UI.getCurrent().getInternals().getTitle();
+        assertThat(pageTitle).isEqualTo("Home");
     }
 }
