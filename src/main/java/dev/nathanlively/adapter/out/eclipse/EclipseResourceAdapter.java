2d0cbd13f9521e9d40ca72c270017997c6331ebd
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-09-10 11:27:06 -0400

    timesheet entries!

--- src/main/java/dev/nathanlively/adapter/out/eclipse/EclipseResourceAdapter.java
+++ src/main/java/dev/nathanlively/adapter/out/eclipse/EclipseResourceAdapter.java
@@ -2,7 +2,9 @@ package dev.nathanlively.adapter.out.eclipse;
 
 import dev.nathanlively.application.port.ResourceRepository;
 import dev.nathanlively.domain.DataRoot;
+import dev.nathanlively.domain.Project;
 import dev.nathanlively.domain.Resource;
+import dev.nathanlively.domain.TimesheetEntry;
 import org.eclipse.serializer.persistence.types.Storer;
 import org.eclipse.store.integrations.spring.boot.types.concurrent.Read;
 import org.eclipse.store.integrations.spring.boot.types.concurrent.Write;
@@ -39,6 +41,12 @@ public class EclipseResourceAdapter implements ResourceRepository {
     public Optional<Resource> findByEmail(String resourceEmail) {
         DataRoot root = (DataRoot) storageManager.root();
         return Optional.ofNullable(root.resources().byEmail(resourceEmail));
+    }
+
+    @Override
+    public List<TimesheetEntry> timesheetEntriesByProject(Project project) {
+        DataRoot root = (DataRoot) storageManager.root();
+        return root.resources().timesheetEntriesByProject(project);
     }
 
     private void saveWithEagerStoring(DataRoot root) {
--- src/main/java/dev/nathanlively/application/TimesheetEntriesPerProject.java
+++ src/main/java/dev/nathanlively/application/TimesheetEntriesPerProject.java
@@ -1,5 +1,30 @@
 package dev.nathanlively.application;
 
+import dev.nathanlively.application.port.ProjectRepository;
+import dev.nathanlively.application.port.ResourceRepository;
+import dev.nathanlively.domain.Project;
+import dev.nathanlively.domain.TimesheetEntry;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
 public class TimesheetEntriesPerProject {
-    // Start at Project or Resource?
+    private final ResourceRepository repository;
+    private final ProjectRepository projectRepository;
+
+    public TimesheetEntriesPerProject(ResourceRepository repository, ProjectRepository projectRepository) {
+        this.repository = repository;
+        this.projectRepository = projectRepository;
+    }
+
+    public List<TimesheetEntry> with(String projectName) {
+        Objects.requireNonNull(projectName, "Project name cannot be null.");
+        Optional<Project> optionalProject = projectRepository.findByName(projectName);
+        if (optionalProject.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return null;
+    }
 }
